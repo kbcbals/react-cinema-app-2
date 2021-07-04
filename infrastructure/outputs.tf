33391be7bfe2c02b5e3cd452e8f20d4da240fc5e
Balamurugan Balasubramanian <kbcbala@gmail.com>
2021-07-04 12:07:24 +0100

    Added circle ci config.yml manually

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,15 +1,7 @@
 output "cinema_app_bucket_name" {
- value = aws_s3_bucket.cinema_s31_bucket.id
-}
-
-output "cloud_distribution_id" {
- value = aws_cloudfront_distribution.s3_distribution.id
-}
-
-/* variable "cinema_app_bucket_name" {
   value = aws_s3_bucket.cinema_s31_bucket.id
 }
 
-variable "cloud_distribution_id" {
+output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.s3_distribution.id
-} */
+}
